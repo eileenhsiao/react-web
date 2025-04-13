d27e0c0eacca05be0f3f61d7b41af6b88c213a18
eileenhsiao <s111219026@stu.ntue.edu.tw>
2025-04-13 19:36:07 +0800

    map

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,15 +10,26 @@ function Home() {
   return (
     <div className="container mx-auto main-layout min-h-screen">
       <Helmet>
-          <title>{title}</title>
-        </Helmet>
-      <Header/>
+        <title>{title}</title>
+      </Helmet>
+      <Header />
       <div style={{ marginTop: '10px', padding: '0px' }}>
-      <img className="w-full content" src="/img/home.svg" alt="logo"/>  
+        <img className="w-full content" src="/img/home.svg" alt="logo" />
       </div>
-      
+
       <ProductList products={products} className="content" />
-     {/*map*/}
+      {/*map*/}
+      <div className="w-full h-[400px]">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3615.2770044097538!2d121.54220407488506!3d25.024671838667988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442aa2e94f1cc1b%3A0x824de6c6ac711612!2zMTA25Y-w5YyX5biC5aSn5a6J5Y2A5ZKM5bmz5p2x6Lev5LqM5q61!5e0!3m2!1szh-TW!2stw!4v1744543937327!5m2!1szh-TW!2stw"
+          width="100%"
+          height="300"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
       <Footer className="footer" />
     </div>
 
