19ac0b6f148c56b607776ead8bc0eb18025dac8a
jieyiy <s111219033@stu.ntue.edu.tw>
2025-04-13 22:53:35 +0800

    aboutus

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,13 +6,13 @@ import Footer from '@/components/Footer'
 function About() {
   const title = "關於我們";
     return (
-      <div className="container mx-auto main-layout bg-gray-900 min-h-screen">
+      <div className="container mx-auto main-layout min-h-screen">
         <Helmet>
             <title>{title}</title>
           </Helmet>
         <Header/>
         <div style={{ marginTop: '61.6px', padding: '0px' }}>
-          <img className="w-full" src="/img/aboutus.svg" alt="AboutUs" />
+          <img className="w-full content" src="/img/aboutus.svg" alt="AboutUs" />
         </div>
         
         <Footer className="footer" />
