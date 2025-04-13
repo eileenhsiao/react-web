cb3a7550195d33c35bdfecfda160b8a35b6d83ea
jieyiy <s111219033@stu.ntue.edu.tw>
2025-04-13 16:18:32 +0800

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,14 +33,21 @@ export default function Header() {
                 </div>
             </div>
             {/*桌面*/}
-            <header className="relative text-center flex flex-col items-center header sticky-header">
-                <Link to="/">
-                    <img className="w-full" src="/img/logo.svg" alt="logo" />
-                </Link>
-                <Link to="/about">關於我們</Link>
-                <Link to="/products/list/all">商品列表</Link>
-                <SetColorMode />
-                <CartSummary />
+            <header className="relative text-center f header sticky-header">
+                <div className="flex items-center justify-between px-4 py-2">
+                    {/* 左邊的 logo */}
+                    <Link to="/">
+                        <img className="h-6" src="/img/logo.svg" alt="logo" />
+                    </Link>
+
+                    {/* 右邊的導航區塊 */}
+                    <div className="flex items-center gap-x-6">
+                        <Link to="/about">關於我們</Link>
+                        <Link to="/products/list/all">商品列表</Link>
+                        <SetColorMode />
+                        <CartSummary />
+                    </div>
+                </div>
             </header>
 
 
