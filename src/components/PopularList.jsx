e762b26a7537413897b84c0d62716ef39fa6fd9c
jieyiy <s111219033@stu.ntue.edu.tw>
2025-04-14 23:31:10 +0800

    popular

--- src/components/PopularList.jsx
+++ src/components/PopularList.jsx
@@ -3,7 +3,9 @@ import { Link } from "react-router";
 
 function PopularList({ products }) {
    return (
-      <div className="w-full content mt-10">
+      <div className="w-full content mt-10 text-center">
+         <h1 className="text-2xl  font-bold mb-10 bottom-border inline-block mx-auto">熱門商品</h1>
+
          <div className="max-w-5xl mx-auto grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8 px-4">
             {products.slice(0, 6).map((product) => (
                <ProductItem key={product.id} product={product} />
