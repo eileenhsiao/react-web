fe7b0fa8a9f8b19a825b15d59238aa85be6768c5
jieyiy <s111219033@stu.ntue.edu.tw>
2025-04-15 16:13:07 +0800

    list

--- src/components/PopularList.jsx
+++ src/components/PopularList.jsx
@@ -7,7 +7,7 @@ function PopularList({ products }) {
       <div className="w-full content mt-10 text-center">
          <h1 className="text-2xl  font-bold mb-10 bottom-border inline-block mx-auto">熱門商品</h1>
 
-         <div className="max-w-5xl mx-auto grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8 px-4">
+         <div className="max-w-5xl mx-auto grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8 px-8 md:px-4">
             {products.slice(0, 6).map((product) => (
                <ProductItem key={product.id} product={product} />
             ))}
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,7 +3,7 @@ import ProductItem from '@/components/ProductItem';
 function ProductList({ products }) {
    return (
       <div className="w-full content mt-10">
-         <div className="max-w-5xl mx-auto grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8 px-4 mb-20">
+         <div className="max-w-5xl mx-auto grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8 px-8 md:px-4 mb-20">
             {products.map((product) => (
                <ProductItem key={product.id} product={product} />
             ))}
