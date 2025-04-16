c3746938ac9bd65560e36c85480be35780fb376d
jieyiy <s111219033@stu.ntue.edu.tw>
2025-04-16 20:02:46 +0800

    text

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -8,11 +8,11 @@ function ProductItem({ product }) {
       <section className="w-full max-w-[280px] mx-auto pt-4 px-3 lg:px-4 ">
          <div className="custom-border overflow-hidden pbox">
             <Link to={`/products/id/${product.id}`}>
-               <img className="w-full max-w-[250px] mx-auto text-sm md:text-lg" src={product.image} alt={product.name} />
+               <img className="w-full max-w-[250px] mx-auto " src={product.image} alt={product.name} />
             </Link>
             <Link to={`/products/id/${product.id}`}>
             <div className="p-2">
-               <h5 className="mb-1 text-center c-text">
+               <h5 className="mb-1 text-center c-text text-xs md:text-base">
                   {product.name}
                </h5>         
             </div>
