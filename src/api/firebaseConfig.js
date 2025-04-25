0a1430b7805aac21af49b36fffb290c81ea308d9
eileenhsiao <s111219026@stu.ntue.edu.tw>
2025-04-25 14:51:55 +0800

    firebase

---
+++ src/api/firebaseConfig.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getApps, getApp, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APPID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID,
+};
+
+const app_length = getApps().length > 0;
+
+// Initialize Firebase
+const app = app_length ? getApp() : initializeApp(firebaseConfig);
+
+// REFERENCE DB
+const db = getFirestore(app);
+
+// REFERENCE AUTH
+const auth = getAuth(app);
+
+export { db, auth };
+const analytics = getAnalytics(app);
