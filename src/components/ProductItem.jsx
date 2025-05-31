17ca8d819096f1ec6191c97efc8e847b2fc10352
jieyiy <s111219033@stu.ntue.edu.tw>
2025-05-31 09:07:43 +0800

    ani

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -3,20 +3,25 @@ import { Link } from 'react-router';
 import _ from 'lodash';
 import '@/index.css';
 
-
 function ProductItem({ product }) {
    return (
-      <section className="w-full max-w-[280px] mx-auto pt-4 px-3 lg:px-4 ">
+      <section className="w-full max-w-[280px] mx-auto pt-4 px-3 lg:px-4">
          <div className="custom-border overflow-hidden pbox">
             <Link to={`/products/id/${product.id}`}>
-               <img className="w-full max-w-[250px] mx-auto " src={product.image} alt={product.name} />
+               <div className="overflow-hidden">
+                  <img
+                     className="w-full max-w-[250px] mx-auto transition-transform duration-300 ease-in-out hover:scale-115"
+                     src={product.image}
+                     alt={product.name}
+                  />
+               </div>
             </Link>
             <Link to={`/products/id/${product.id}`}>
-            <div className="p-2">
-               <h5 className="mb-1 text-center c-text text-xs md:text-base">
-                  {product.name}
-               </h5>         
-            </div>
+               <div className="p-2">
+                  <h5 className="mb-1 text-center c-text text-xs md:text-base">
+                     {product.name}
+                  </h5>
+               </div>
             </Link>
          </div>
       </section>
