278939b0941b586d5213417f3bd27aa677e8ee74
eileenhsiao <s111219026@stu.ntue.edu.tw>
2025-06-01 10:31:59 +0800

    payment

--- src/components/ShippingAddressCard.jsx
+++ src/components/ShippingAddressCard.jsx
@@ -23,7 +23,7 @@ export default function ShippingAddressCard() {
     // 儲存付款方式與地址
     dispatch(saveShippingAddress(values));
     dispatch(savePaymentMethod(values.paymentMethod));
-    
+
   };
 
   return (
@@ -55,7 +55,7 @@ export default function ShippingAddressCard() {
         initialValues={{ ...shippingAddress, paymentMethod: paymentMethod }}
       >
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-20">
-          
+
           {/* 左欄：收件人資料 + 付款方式 */}
           <div className="flex flex-col justify-between h-full md:pr-8 md:border-r-2 md:border-primary">
             <div>
@@ -73,102 +73,157 @@ export default function ShippingAddressCard() {
               </Form.Item>
 
               <Form.Item
-                
+
                 name="phonenumber"
                 rules={[
                   { type: "string" },
-                  { required: true, message: "電話" },
+                  { required: true, message: "請輸入聯絡電話" },
                 ]}
                 hasFeedback
               >
-                <Input placeholder="請輸入聯絡電話" />
+                <Input placeholder="電話" />
               </Form.Item>
             </div>
 
             <div className="mt-8">
-              
+
 
               {shippingMethod === "pickup" && (
-                  <div>
-                    <div className="content text-xl mb-4">取件資料</div>
-                      <Form.Item name="customPayCode" 
-                          rules={[
-                              {
-                                  type: "string",
-                              },
-                              {
-                                  required: true,
-                                  message: "Please input your card number",
-                              },
-                          ]}
-                          hasFeedback
-                      >
-                          <Input placeholder="Enter card number" autoComplete="off"/>
-                      </Form.Item>
-                  </div>
+                <div>
+                  <div className="content text-xl mb-4">取件資料</div>
+                  <Form.Item name="pickupShop"
+                    rules={[
+                      {
+                        type: "string",
+                      },
+                      {
+                        required: true,
+                        message: "請輸入取貨分店",
+                      },
+                    ]}
+                    hasFeedback
+                  >
+                    <Input placeholder="取貨分店" autoComplete="off" />
+                  </Form.Item>
+                  <Form.Item name="pickupDate"
+                    rules={[
+                      {
+                        type: "string",
+                      },
+                      {
+                        required: true,
+                        message: "請輸入取貨日期",
+                      },
+                    ]}
+                    hasFeedback
+                  >
+                    <Input placeholder="日期" autoComplete="off" />
+                  </Form.Item>
+                  <Form.Item name="pickupTime"
+                    rules={[
+                      {
+                        type: "string",
+                      },
+                      {
+                        required: true,
+                        message: "請輸入取貨時間",
+                      },
+                    ]}
+                    hasFeedback
+                  >
+                    <Input placeholder="時間" autoComplete="off" />
+                  </Form.Item>
+                </div>
               )}
               {shippingMethod === "home" && (
-                  <div>
-                    <div className="content text-xl mb-4">宅配資料</div>
-                      <Form.Item name="customPayCode" 
-                          rules={[
-                              {
-                                  type: "string",
-                              },
-                              {
-                                  required: true,
-                                  message: "Please input your card number",
-                              },
-                          ]}
-                          hasFeedback
-                      >
-                          <Input placeholder="Enter card number" autoComplete="off"/>
-                      </Form.Item>
-                  </div>
+                <div>
+                  <div className="content text-xl mb-4">宅配資料</div>
+                  <Form.Item name="address"
+                    rules={[
+                      {
+                        type: "string",
+                      },
+                      {
+                        required: true,
+                        message: "請輸入宅配地址",
+                      },
+                    ]}
+                    hasFeedback
+                  >
+                    <Input placeholder="宅配地址" autoComplete="off" />
+                  </Form.Item>
+                </div>
               )}
             </div>
           </div>
 
           {/* 右欄：其他收件資訊 */}
           <div className="flex flex-col justify-between h-full">
-            
-            <div className="content text-xl mb-4">付款資料</div>  
-            <Form.Item
-              label="City"
-              name="city"
-              rules={[{ required: true, message: "請輸入城市" }]}
+
+            <div className="content text-xl mb-4">付款資料</div>
+            <Form.Item name="cardnumber"
+              rules={[
+                {
+                  type: "string",
+                },
+                {
+                  required: true,
+                  message: "請輸入信用卡卡號",
+                },
+              ]}
               hasFeedback
             >
-              <Input placeholder="Enter city" />
+              <Input placeholder="信用卡卡號" autoComplete="off" />
+            </Form.Item>
+            <Form.Item name="cardDate"
+              rules={[
+                {
+                  type: "string",
+                },
+                {
+                  required: true,
+                  message: "請輸入信用卡有效期限",
+                },
+              ]}
+              hasFeedback
+            >
+              <Input placeholder="有效期限" autoComplete="off" />
+            </Form.Item>
+            <Form.Item name="cardsafenumber"
+              rules={[
+                {
+                  type: "string",
+                },
+                {
+                  required: true,
+                  message: "請輸入信用卡安全碼",
+                },
+              ]}
+              hasFeedback
+            >
+              <Input placeholder="安全碼" autoComplete="off" />
             </Form.Item>
 
-            <Form.Item
-              label="Postal Code"
-              name="postalCode"
-              rules={[{ required: true, message: "請輸入郵遞區號" }]}
-              hasFeedback
-            >
-              <Input placeholder="Enter postal code" />
-            </Form.Item>
 
-            <Form.Item
-              label="Country"
-              name="country"
-              rules={[{ required: true, message: "請輸入國家" }]}
-              hasFeedback
-            >
-              <Input placeholder="Enter country" />
-            </Form.Item>
             <div className="content text-xl mb-4">其他</div>
+            <Form.Item name="other"
+              rules={[
+                {
+                  type: "string",
+                }
+              ]}
+            >
+              <Input placeholder="備註（例：需要附上餐具、蠟燭）"/>
+            </Form.Item>
             <button
-             onClick={() => {
-              
-               navigate("/SendOrder"); // 導向下一頁
-                }}
-                className="w-full bg-primary text-white py-3 rounded mt-6"
-                >
-                送出訂單
-                </button>                       
+              onClick={() => {
+
+                navigate("/SendOrder"); // 導向下一頁
+              }}
+              className="w-full bg-primary text-white py-3 rounded mt-6"
+            >
+              送出訂單
+            </button>
           </div>
 
         </div>
